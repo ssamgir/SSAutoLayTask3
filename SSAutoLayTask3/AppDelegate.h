195d9a7f95a517f98a0f629756666dcdc0130a42
amitbobade2007 <amitbobade2007@gmail.com>
2016-07-31 19:31:23 +0530

    initial comit

---
+++ SSAutoLayTask3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSAutoLayTask3
+//
+//  Created by Amit Bobade on 31/07/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSAutoLayTask3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSAutoLayTask3
+//
+//  Created by Amit Bobade on 31/07/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
